2674be9ecae0e6ba183062ab37b80418cea3f2c4
alon1525 <alon1525@gmail.com>
2024-06-10 14:31:52 +0300

    checks now if it has been added already or not exist

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
+
 const capitalizeWords = (string) => {
   if (!string) return "";
   return string
@@ -30,11 +41,7 @@ const capitalizeWords = (string) => {
 app.get("/", async (req, res) => {
   //Write your code here.
   try {
-    const result = await db.query("SELECT country_code FROM visited_countries");
-    let countries = [];
-    result.rows.forEach((country) => {
-      countries.push(country.country_code);
-    });
+    const countries = await checkVisisted();
     res.render("index.ejs", {
       countries: countries,
       total: countries.length,
@@ -47,24 +54,39 @@ app.get("/", async (req, res) => {
 app.post("/add", async (req, res) => {
   try {
     const input = req.body["country"];
-    const tag = await db.query(
+    const result = await db.query(
       "SELECT country_code FROM countries WHERE country_name = $1",
       [capitalizeWords(input)]
     );
-    console.log(tag.rows);
-    if (tag.rows.length !== 0) {
-      const countryCode = tag.rows[0].country_code;
-      console.log(countryCode);
-      await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
-        countryCode,
-      ]);
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
+      );
       res.redirect("/");
+    }catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
     }
   } catch (err) {
-    console.error("Error inserting country code into", err);
+    console.log(err);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
   }
 });
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
